3955a29a8c7c5eaf6a08a4f942441fd0546d1b58
dianamilova <milova.diana.94.94@mail.ru>
2022-12-26 13:41:05 +0700

    new

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)  {
+       int balance = 200;
+       int refill = 1000;
+       int percent;
+       if (refill >=1000) {
+           percent= 1;
+        } else {
+           percent= 0;
+        }
+        int bonus = refill / 100* percent;
+        int totalBalance = balance + refill + bonus;
+
+        System.out.println("Баланс составляет:"+ totalBalance);
+
+  }
+}
