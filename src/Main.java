7279c552ac47ba65a0fef20866caa68a658bd070
dianamilova <milova.diana.94.94@mail.ru>
2022-12-27 10:13:38 +0700

    change file

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
-    public static void main(String[] args)  {
-       int balance = 200;
-       int refill = 1000;
-       int percent;
-       if (refill >=1000) {
-           percent= 1;
+    public static void main(String[] args) {
+
+        int balance = 200;
+        int refill = 1000;
+        int percent;
+        if (refill >= 1000) {
+            percent = 1;
         } else {
-           percent= 0;
+            percent = 0;
         }
-        int bonus = refill / 100* percent;
+        int bonus = refill / 100 * percent;
         int totalBalance = balance + refill + bonus;
 
-        System.out.println("Баланс составляет:"+ totalBalance);
+        System.out.println("Баланс составляет:" + totalBalance);
 
-  }
+    }
 }
